df785d67ac4235ffe7b498df7f54e4361c5aadaa
Sebastian Lorenz <50888514+SebastianLorenz@users.noreply.github.com>
2020-02-24 22:27:28 +0100

    Update preteceni.cpp

--- cv2/preteceni.cpp
+++ cv2/preteceni.cpp
@@ -8,10 +8,8 @@ int main() {
     short a, b, c; // Zmente typ na int, vyzkousejte i unsigned variantu
     
     cout << "Celociselne preteceni a+b" << endl;
-    cout << "Zadejte cislo a: ";        // Zadejte treba 32000
-    cin >> a;   // Cteni z konzole do promenne 'a'
-    cout << "Zadejte cislo b: ";        // Zadejte treba 10000
-    cin >> b;   // Cteni z konzole do promenne 'b'
+    a = 32000; // zadejte prvni cislo (treba 32000)
+    b = 11000; // zadejte druhe cislo (treba 11000)
 
     c = a + b;  // Nesmyslny vysledek pro typ short
     
